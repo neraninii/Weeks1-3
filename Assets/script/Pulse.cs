99bf28c381a4658dd8d000e4bda2c93191381ad9
Kinnera Sateesh <sateeshk@shernet.sheridancollege.ca>
2026-01-12 10:13:47 -0500

    Added an anim curve, pulse, etc

--- Assets/script/Pulse.cs
+++ Assets/script/Pulse.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 
-public class LinearInterpolation : MonoBehaviour
+public class Pulse : MonoBehaviour
 {
-    public Transform start;
-    public Transform end;
-    public float t = 0;
-
     public AnimationCurve curve;
+    public float t = 0; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +15,13 @@ public class LinearInterpolation : MonoBehaviour
     void Update()
     {
         t += Time.deltaTime;
-        if(t > 1)
+        if (t > 1)
         {
             t = 0;
         }
-
-        //transform.position = Vector2.Lerp(start.position, end.position, t);
-        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+        float y = curve.Evaluate(t);
+        //Vector3.one is the sa,e as a vector with 1s in every column (1,1,1)
+        Vector3 newSize = Vector3.one * y;
+        transform.localScale = Vector3.one * curve.Evaluate(t);
     }
 }
